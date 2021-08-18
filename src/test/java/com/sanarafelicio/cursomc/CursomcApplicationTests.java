e983ec3535b438f83e37415555bbaef0386b41f1
Sanara Felicio <sanarafm27@gmail.com>
2021-08-18 14:53:05 -0300

    projeto criado

---
+++ src/test/java/com/sanarafelicio/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanarafelicio.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
